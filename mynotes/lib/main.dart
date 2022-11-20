5a21bc17537f5e58492c394b275f7b91ea0f9ebb
Muhammad Rafli Afriansyah Ikhsan <nashkihaysnairfailfardammahum@gmail.com>
2022-11-20 18:18:32 +0800

    Finished chapter 14: Separating app initialization from login/register

---
+++ mynotes/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tes/firebase_options.dart';
+import 'package:tes/views/login_view.dart';
+
+void main(List<String> args) {
+  WidgetsFlutterBinding.ensureInitialized;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo Home Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Homepage'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('You are a verified user');
+              } else {
+                print('You need to verify your email first');
+              }
+              return Text('Done');
+            default:
+              return Text('Loading...');
+          }
+        },
+      ),
+    );
+  }
+}
